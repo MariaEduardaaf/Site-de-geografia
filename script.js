7a0573d7ba9d13753749dbd7c47bf1732c05db50
Maria Eduarda <maria.eduarda@grupoviadigital.com>
2022-10-24 13:36:19 -0300

    ooo

--- script.js
+++ script.js
@@ -3,65 +3,55 @@ let pergunta = [
         titulo: "1-Os 92 imigrantes na fronteira da turquia possuíam documentos de identificação?",
         alternativa: [
             "Não",
-            "sim", 
-            "alguns sim",
-            
+            "sim",
+            "Alguns sim",
+
         ],
         correta: 0
     }, {
-        titulo: "2-As pessoas possuíam oque no corpo?",
+        titulo: "2-As pessoas possuíam o que no corpo?",
         alternativa: [
-            "tatuagens", 
-            "bolsas com roupas", 
-            "ferimentos no corpo",
-            "nenhuma das alternativas"
+            "Tatuagens",
+            "Bolsas com roupas",
+            "Ferimentos no corpo",
         ],
         correta: 2
     }, {
         titulo: "3-As famílias viam para o Brasil como?",
         alternativa: [
-            "com toda sua família ", 
-            "apenas com dinheiro da passagem", 
-            "apenas com a roupa do corpo ",
-            "nenhuma das alternativas"
+            "Com toda sua família ",
+            "Apenas com dinheiro da passagem",
+            "Apenas com a roupa do corpo ",
         ],
         correta: 1
-    }, {
-        titulo: "4-Donald Trump queria fazer oque com os pais e seus filhos?",
+    }, 
+    {
+        titulo: "4-Joe Biden disse que iria tratar as pessoas como?",
         alternativa: [
-            "separar os filhos dos pais", 
-            "mandar os pais de volta e deixar as crianças", 
-            "expulsar todos eles",
-            "nenhuma das alternativas"
+            "Iria expulsar todos",
+            "Não ia tratar com respeito",
+            "Iria tratar com humanidade e respeito",
+        ],
+        correta: 2
+    },
+    {
+        titulo: "5-Sobre os governos:",
+        alternativa: [
+            "Muitos estão falhando nas suas obrigações morais e legais de defender os mais vuneráveis",
+            "Estão sendo totalmente correto na poítica com migrantes",
+            "Estão cumprindo suas obrigações morais e legais, defendendo os mais vuneráveis",
         ],
         correta: 0
     },
     {
-        titulo: "5-Donald Trump conseguiu separar os pais dos seus filhos?",
-        alternativa: [
-            "Sim", 
-            "Não", 
-        ],
-        correta: 1
-    },
-    {
-        titulo: "6-Joe Biden disse que ia tratar as pessoas como?",
-        alternativa: [
-            "ia expulsar todos", 
-            "não ia tratar com respeito", 
-            "ia tratar com humanidade e respeito",
-            "nenhuma das alternativas"
-        ],
-        correta: 2
-    },
-    {
         titulo: "Fim de jogo",
         alternativa: [
-            "espero nque tenha gostado"
+            "Espero que tenham gostado",
+            "Deixe seu like",
+            "Se inscreva no canal"
         ],
-        correta: 2
     }
-    
+
 ];
 
 let app = {
@@ -91,7 +81,7 @@ let app = {
     },
 
     proximaperg: function () {
-        this.atualpos ++;
+        this.atualpos++;
         if (this.atualpos == pergunta.length) {
             this.atualpos = 0
         }
@@ -101,7 +91,7 @@ let app = {
     checarResposta: function (user) {
         if (this.qatual.correta == user) {
             console.log("correta")
-            this.totalpontos ++;
+            this.totalpontos++;
         } else {
             console.log("errada")
             this.mostraresposta()
@@ -114,28 +104,18 @@ let app = {
 
     atualizarPontos: function () {
         let scoreDiv = document.getElementById("pontos");
-        scoreDiv.textContent = `Sua pontuação é: ${
-            this.totalpontos
-        }`;
+        scoreDiv.textContent = `Sua pontuação é: ${this.totalpontos
+            }`;
     },
 
     mostraresposta: function (correto) {
         let resultDiv = document.getElementById("result");
         let result = ""
-        //formatar com a mensagem será exibida
-        //o erro esta aqui
-        if(correto == true) {
-            result = "Resposta correta";
-        } else {
-            let perguntas = pergunta[this.atualpos];
-            //obtenha o indice da resposta correta da questão atual
-            let indice = perguntas.correta;
-            //obtenha o texto da resposta correta da quatão atual
-            let ctexto = perguntas.alternativa[indice];
-            result = `Incorreto! Resposta Correta: ${ctexto}`
-        }
+        if (correto == true) {
+            result = `Resposta Correta`;
+        } 
         resultDiv.textContent = result;
-}
+    }
 
 }
 app.start();
